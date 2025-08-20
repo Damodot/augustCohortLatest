63305776fbc28506270f7f1224763abb38b602f6
damodotun <damodotunadedeji@gmail.com>
2025-08-20 10:34:50 +0100

    First Commit

---
+++ introduction.ts
@@ -0,0 +1,17 @@
+type user = {
+    username: string,
+    password: string
+}
+console.log('Hello');
+
+let allUser:Array<user> = [];
+
+let obj = {
+    username: 'Adedotun',
+    password: 'ADeshstrj',
+    pin: 41241
+}
+
+allUser.push(obj)
+
+console.log(allUser);
